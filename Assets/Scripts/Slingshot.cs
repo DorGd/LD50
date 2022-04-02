ad16d0589d4103623346dee8b274533163325d2d
DorGd <dor.gid00@gmail.com>
2022-04-02 21:37:04 +0300

    Adjusting slingshot

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -1,24 +1,36 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Slingshot : MonoBehaviour
 {
-    public Vector3 originPosition;
-    
-    public Vector3 currentPosition;
 
     public LineRenderer lineRenderer;
 
     public int multiplier;
     
-    private bool _isPressed;
+    private Vector3 _originPosition;
     
+    private Vector3 _currentPosition;
+    
+    private bool _isPressed;
+
+    private Rigidbody _selectedRigidBody;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
     float AngleBetweenTwoPoints() {
         //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+        Vector2 positionOnScreen = _selectedRigidBody.position /*_camera.WorldToViewportPoint (transform.position)*/;
          
         //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector2 mouseOnScreen = _currentPosition/*_camera.ScreenToViewportPoint(Input.mousePosition)*/;
         
         return Mathf.Atan2(positionOnScreen.y - mouseOnScreen.y, positionOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
     }
@@ -29,25 +41,30 @@ public class Slingshot : MonoBehaviour
     
         if (Input.GetButtonDown("Fire1"))
         {
-            originPosition = new Vector3(mousePos.x, mousePos.x, 1);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+            if (Physics.Raycast(ray, out hit)) {
+                _selectedRigidBody = hit.rigidbody;
+                _originPosition = hit.transform.position;
+                _isPressed = true;
+            }
+            
     
-            _isPressed = true;
         }
         
-        if (Input.GetButton("Fire1"))
+        if (_isPressed && Input.GetButton("Fire1"))
         {
-            currentPosition = new Vector3(mousePos.x, mousePos.x, 1);
-    
-            var direction = (currentPosition - originPosition) / multiplier;
+            _currentPosition = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,Mathf.Abs(_camera.transform.position.z - _selectedRigidBody.position.z)));
+            var direction = _currentPosition - _originPosition;
             
             transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,AngleBetweenTwoPoints()));
-
-            Debug.Log(direction);
             
-            lineRenderer.SetPosition(1, direction);
+            // lineRenderer.SetPosition(1, direction);
+            Debug.DrawLine(_originPosition, _originPosition + direction, Color.magenta);
         }
         
-        if (Input.GetButtonUp("Fire1"))
+        if (_isPressed && Input.GetButtonUp("Fire1"))
         {
             _isPressed = false;
     
@@ -57,6 +74,7 @@ public class Slingshot : MonoBehaviour
 
     private void SlingHumanToSpace()
     {
-        
+        var direction = (_currentPosition - _originPosition) * multiplier;
+        _selectedRigidBody.AddForce(-direction);
     }
 }
